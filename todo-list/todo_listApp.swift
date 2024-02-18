f4eb6ce36c5ce957d7147e917d44f29f1e29819b
Susumu Hoshikawa <hoshi005@me.com>
2024-02-18 10:28:01 +0900

    first commit

---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by Susumu Hoshikawa on 2024/02/18.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
